606e9b096b459325c0fd3c7d5f70bab3a0c91eb6
Bebroros <retrosab@gmail.com>
2024-11-05 22:58:12 +0200

    ]

--- Hacking.py
+++ Hacking.py
@@ -26,18 +26,22 @@ def main():
         for word in list_of_random_words:
             print(word)
         password = random.choice(list_of_passwords)
-        for x in range(4, 0, -1):
+        for x in range(4, -1, -1):
+            if x == 0:
+                print('Access Denied')
+                print('Password was:', password)
+                break
             guess = input(f'Enter password: ({x} tries remaining)')
-            if guess == password:
+            if guess.upper() == password:
                 print('A C C E S S G R A N T E D')
+                break
             else:
                 similarity_count = 0
-                for letter in set(list(guess.upper())):
+                bebra = list(set(list(guess.upper())))
+                for letter in list(set(list(guess.upper()))):
                     if letter in password:
                         similarity_count += 1
                 print(f"Access Denied({similarity_count}/7 correct)")
-        else:
-            print('You`ve lost :(')
         if input('Type (N) if you want to quit').upper() == 'N':
             break
 
